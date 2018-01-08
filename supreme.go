7791ffb250058ce80116dabcb8b7b954593fa325
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-09 02:03:55 +0500

    The last checkout prob

--- supreme.go
+++ supreme.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
 
 	"github.com/parnurzeal/gorequest"
 )
@@ -78,7 +80,7 @@ func main() {
 		Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:57.0) Gecko/20100101 Firefox/57.0").
 		Set("Content-Type", "application/x-www-form-urlencoded").
 		Set("Accept-Encoding", "gzip, deflate, br").
-		Set("Referer", "https://www.supremenewyork.com/checkout").
+		Set("Referer", "https://www.supremenewyork.com").
 		Set("Origin", "https://www.supremenewyork.com").
 		Set("Connection", "keep-alive").
 		Set("Accept", "application/json").
@@ -89,6 +91,7 @@ func main() {
 	// Set("X-CSRF-Token", token).
 	if errs != nil {
 		log.Println(errs)
+		os.Exit(1)
 	}
 	// req, body, errs := request.Get("https://www.supremenewyork.com").End()
 	// fmt.Println("REsp is " + resp.Status)
@@ -97,5 +100,82 @@ func main() {
 	// 	fmt.Print(errs)
 	// }
 	// resp.Body.Close()
-	fmt.Print(body)
+	fmt.Print("Status of the item is " + body)
+
+	_, body, err := request.Get("http://www.supremenewyork.com/checkout").End()
+
+	if err != nil {
+		log.Println(errs)
+		os.Exit(1)
+	}
+	pageOutput := body
+	i := strings.Index(pageOutput, "csrf-token")
+	chars := pageOutput[i:]
+	x := strings.Index(chars, " />")
+	temp := chars[:x]
+	temp = temp[21:]
+	g := len(temp)
+	temp = temp[:(g - 1)]
+	token := temp
+	fmt.Println("After getting the checkout page " + body)
+	checkoutinfo := CheckoutInfo{
+		AuthenticityToken: token,
+		BillingName:       "",
+		Email:             "", //string@domain.com
+		Tel:               "",
+		Address1:          "",
+		City:              "",
+		Zip:               "",
+		Country:           "",
+		SameAsBilling:     true,
+		CardType:          "visa",
+		CNB:               "", // 1111 1111 1111 1111
+		Month:             "", // 01
+		Year:              "", // 2020
+		VVal:              "", //123
+		Terms:             true,
+		Captcha:           "",
+	}
+	m = map[string]interface{}{
+		"utf8":                     "✓",
+		"authenticity_token":       token,
+		"order[billing_name]":      checkoutinfo.BillingName,
+		"order[email]":             checkoutinfo.Email,
+		"order[tel]":               checkoutinfo.Tel,
+		"order[billing_address]":   checkoutinfo.Address1,
+		"order[billing_address_2]": "",
+		"order[billing_address_3]": "",
+		"order[billing_city]":      checkoutinfo.City,
+		"order[billing_zip]":       checkoutinfo.Zip,
+		"order[billing_country]":   checkoutinfo.Country,
+		"same_as_billing_address":  "1",
+		"store_credit_id":          "",
+		"credit_card[type]":        checkoutinfo.CardType,
+		"credit_card[cnb]":         checkoutinfo.CNB,
+		"credit_card[month]":       checkoutinfo.Month,
+		"credit_card[year]":        checkoutinfo.Year,
+		"credit_card[vval]":        checkoutinfo.VVal,
+		"order[terms]":             "0",
+		"hpcvv":                    "",
+		"cnt":                      "1"}
+
+	_, body1, err1 := request.Get("http://www.supremenewyork.com/checkout.js").
+		Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:57.0) Gecko/20100101 Firefox/57.0").
+		Set("Content-Type", "application/x-www-form-urlencoded").
+		Set("Accept-Encoding", "gzip").
+		Set("Referer", "https://www.supremenewyork.com/checkout").
+		Set("Origin", "https://www.supremenewyork.com").
+		Set("Connection", "keep-alive").
+		Set("Accept", "application/json").
+		Set("X-Requested-Width", "XMLHttpRequest").
+		Send(m).
+		AddCookies(cookies).
+		End()
+
+	if err1 != nil {
+		log.Println(err1)
+		os.Exit(1)
+	}
+	fmt.Print("\nAfter getting JS file " + string(body1))
+
 }
